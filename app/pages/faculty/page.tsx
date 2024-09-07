853c322af432e4de3dfca7adaa890498894c459b
Abubakar Siddique <mvut.usa@gmail.com>
2024-09-07 04:11:43 -0700

    Faculty v1.6

--- app/pages/faculty/page.tsx
+++ app/pages/faculty/page.tsx
@@ -9,14 +9,36 @@ import Arshad from '@/public/faculty/arshad.jpg';
 import Gami from '@/public/faculty/gami.jpg';
 import Abid from '@/public/faculty/abid.jpg';
 import Haris from '@/public/faculty/haris.jpg';
+import Mike from '@/public/faculty/mike2.jpg';
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function FacultyComponent(){
     return(
         <div className={'container mx-auto flex flex-col items-center justify-center py-6'}>
             <p className={'text-lg md:text-3xl font-bold text-center py-2 lg:py-10 uppercase'}>Faculty</p>
             <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-start gap-6 px-6 '}>
+                {/*    */}
+                <div
+                    className={'w-auto h-full grid grid-cols-1 transition ease-in-out delay-150 odd:bg-blue-700 even:bg-teal-700 p-2 hover:-translate-y-1 hover:scale-110 hover:bg-amber-700 rounded-xl duration-300 '}>
+                    <Image src={Mike} alt={''} className={'h-96 w-full'}/>
+                    <h2 className={'font-bold text-lg text-white'}>Mike Litman <Link href={'https://www.mikeversity.com/'} target={'_blank'} className={'text-xs text-amber-200'}>(Profile)</Link> </h2>
+                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white'}>
+                        Global Thought Leader
+                    </p>
+                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white italic'}>United States</p>
+                </div>
+                {/*    */}
+                <div
+                    className={'w-auto h-full grid grid-cols-1 transition ease-in-out delay-150 odd:bg-blue-700 even:bg-red-700 p-2 hover:-translate-y-1 hover:scale-110 hover:bg-amber-700 rounded-xl duration-300 '}>
+                    <Image src={Abs} alt={''} className={'h-96 w-full'}/>
+                    <h2 className={'font-bold text-lg text-white'}>Abubakar Siddique</h2>
+                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white'}>
+                        Software Engineer
+                    </p>
+                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white italic'}>Pakistan</p>
+                </div>
                 {/*    */}
                 <div
                     className={'w-auto h-full grid grid-cols-1 transition ease-in-out delay-150 odd:bg-blue-700 even:bg-teal-700 p-2 hover:-translate-y-1 hover:scale-110 hover:bg-amber-700 rounded-xl duration-300 '}>
@@ -54,16 +76,6 @@ export default function FacultyComponent(){
                     <h2 className={'font-bold text-lg text-white'}>Ghulam Abbas</h2>
                     <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white'}>
                         Sr. Instructor Programming & Algorithms
-                    </p>
-                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white italic'}>Pakistan</p>
-                </div>
-                {/*    */}
-                <div
-                    className={'w-auto h-full grid grid-cols-1 transition ease-in-out delay-150 odd:bg-blue-700 even:bg-red-700 p-2 hover:-translate-y-1 hover:scale-110 hover:bg-amber-700 rounded-xl duration-300 '}>
-                    <Image src={Abs} alt={''} className={'h-96 w-full'}/>
-                    <h2 className={'font-bold text-lg text-white'}>Abubakar Siddique</h2>
-                    <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white'}>
-                        Software Engineer
                     </p>
                     <p className={'text-xs sm:text-sm md:text-md lg:text-lg text-white italic'}>Pakistan</p>
                 </div>
