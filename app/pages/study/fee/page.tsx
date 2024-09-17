cd9dd483eecc4eb22fd0c54a6c355fa3c92bb9ea
Abubakar Siddique <mvut.usa@gmail.com>
2024-09-16 18:29:29 -0700

    Fee updates

--- app/pages/study/fee/page.tsx
+++ app/pages/study/fee/page.tsx
@@ -7,9 +7,9 @@ export default function FeeStructure(){
             <Link href={'/pages/study'} className={'flex text-xs hover:text-red-500 items-center'}><MdArrowBackIos />Back</Link>
 
             <p className={'text-3xl py-10'}>Fee Structure</p>
-            <table className="table-auto text-center text-xs sm:text-lg">
+            <table className="table-auto text-center text-xs sm:text-lg ">
                 <thead>
-                <tr>
+                <tr className={'bg-stone-700 text-gray-50'}>
                     <th>Sr.</th>
                     <th>Program</th>
                     <th>Classes</th>
@@ -20,7 +20,7 @@ export default function FeeStructure(){
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>01</td>
                     <td>Full Stack Development with Next.js (FSDN)</td>
                     <td>Online</td>
@@ -29,7 +29,7 @@ export default function FeeStructure(){
                     <td>$20</td>
                     <td>$530</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>02</td>
                     <td>Office Management and Work Ethics (OM&WE)</td>
                     <td>Online</td>
@@ -38,7 +38,7 @@ export default function FeeStructure(){
                     <td>$20</td>
                     <td>$290</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>03</td>
                     <td>Core Web Technologies (CWT)</td>
                     <td>Online</td>
@@ -47,7 +47,7 @@ export default function FeeStructure(){
                     <td>$20</td>
                     <td>$170</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>04</td>
                     <td>Full Stack PHP Development (FSP)</td>
                     <td>Online</td>
@@ -56,7 +56,7 @@ export default function FeeStructure(){
                     <td>$20</td>
                     <td>$530</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>05</td>
                     <td>Primary School Certificate (PSC)</td>
                     <td>Online</td>
@@ -65,7 +65,7 @@ export default function FeeStructure(){
                     <td>$10</td>
                     <td>$290</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>06</td>
                     <td>Elementary School Certificate (ESC)</td>
                     <td>Online</td>
@@ -74,7 +74,7 @@ export default function FeeStructure(){
                     <td>$13</td>
                     <td>$518</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>07</td>
                     <td>Secondary School Certificate (SSC)</td>
                     <td>Online</td>
@@ -83,7 +83,7 @@ export default function FeeStructure(){
                     <td>$15</td>
                     <td>$410</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>08</td>
                     <td>Higher Secondary School Certificate (HSSC)</td>
                     <td>Online</td>
@@ -92,7 +92,7 @@ export default function FeeStructure(){
                     <td>$16</td>
                     <td>$434</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>09</td>
                     <td>Bachelor of Science in Computer Science (BSCS)</td>
                     <td>Online</td>
@@ -101,7 +101,7 @@ export default function FeeStructure(){
                     <td>$25</td>
                     <td>$1250</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>10</td>
                     <td>PGD in Full Stack Next.js (PGD FSN)</td>
                     <td>Online</td>
@@ -110,7 +110,7 @@ export default function FeeStructure(){
                     <td>$30</td>
                     <td>$410</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>11</td>
                     <td>PGD in Full Stack PHP (PGD FSP)</td>
                     <td>Online</td>
@@ -119,7 +119,7 @@ export default function FeeStructure(){
                     <td>$30</td>
                     <td>$410</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'}>
                     <td>12</td>
                     <td>Master of Science in Full Stack Development MS(FSD)</td>
                     <td>Online</td>
@@ -128,7 +128,7 @@ export default function FeeStructure(){
                     <td>$50</td>
                     <td>$1275</td>
                 </tr>
-                <tr>
+                <tr className={'odd:bg-gray-200 even:bg-teal-100'} >
                     <td>13</td>
                     <td>Ph.D. in Full Stack Development (Ph.D. FSD)</td>
                     <td>Online</td>
