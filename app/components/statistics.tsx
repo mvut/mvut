c6c1671429cfe4cf2cef4ee94c5f87614cc3b930
Abubakar Siddique <mvut.usa@gmail.com>
2024-09-16 17:58:49 -0700

    Statistics updates v2

--- app/components/statistics.tsx
+++ app/components/statistics.tsx
@@ -10,7 +10,7 @@ export default function StatisticsComponent(){
             .then((applicants) => {
                 setApplicants(applicants)
             })
-    }, [applicants]);
+    }, []);
     return(
         <div className={'flex flex-col sm:flex-row bg-teal-950 h-auto items-center justify-center gap-4 py-16'}>
             <div className={'flex flex-col items-center justify-center '}>
@@ -20,16 +20,16 @@ export default function StatisticsComponent(){
 
             <div className={'flex flex-col items-center justify-center '}>
                 <p className={'flex items-center justify-center text-5xl text-gray-50 bg-stone-950 h-24 w-48 rounded-lg'}>100+</p>
-                <p className={'flex items-center justify-center text-xl text-amber-200 bg-indigo-950 h-12 w-52'}>Faculty</p>
+                <p className={'flex items-center justify-center text-xl text-amber-200 bg-indigo-950 h-12 w-52'}>Faculties</p>
             </div>
 
             <div className={'flex flex-col items-center justify-center '}>
-                <p className={'flex items-center justify-center text-5xl text-gray-50 bg-stone-950 h-24 w-48 rounded-lg'}>20+</p>
+                <p className={'flex items-center justify-center text-5xl text-gray-50 bg-stone-950 h-24 w-48 rounded-lg'}>25+</p>
                 <p className={'flex items-center justify-center text-xl text-amber-200 bg-indigo-950 h-12 w-52'}>Programs</p>
             </div>
 
             <div className={'flex flex-col items-center justify-center '}>
-                <p className={'flex items-center justify-center text-5xl text-gray-50 bg-stone-950 h-24 w-48 rounded-lg'}>250+</p>
+                <p className={'flex items-center justify-center text-5xl text-gray-50 bg-stone-950 h-24 w-48 rounded-lg'}>280+</p>
                 <p className={'flex items-center justify-center text-xl text-amber-200 bg-indigo-950 h-12 w-52'}>Courses</p>
             </div>
         </div>
